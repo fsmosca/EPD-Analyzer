2ad35086c87702079b5beb31a813bc3e0f3ecb4b
fsmosca <fsmosca@users.noreply.github.com>
2020-06-02 10:33:44 +0800

    Update run_eap.bat
    * Add sample command line to enable logging.
    * Add sample command line to set Stockfish ending tablebase.

--- run_eap.bat
+++ run_eap.bat
@@ -1,8 +1,17 @@
 set MTMS=1000
-set EPD=./epd/wacnew.epd
+set EPD="./epd/wacnew.epd"
+
+
+:: Enable logging
+eap.exe --log --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128" --outputpgn sf_wacnew_result.pgn --outputepd sf_wacnew_result.epd
+
+
+:: Stockfish uses ending tablebase syzygy
+:: Add SyzygyPath=D:/Chess/syzygy in engineoption
+:: eap.exe --log --input %EPD% --movetimems %MTMS% --engine "D:/Chess/Engines/Stockfish/stockfish_11.exe" --engineoption "Hash=128, SyzygyPath=D:/Chess/syzygy" --outputpgn sf_wacnew_result.pgn --outputepd sf_wacnew_result.epd
 
 
 :: Stockfish
-eap.exe --input %EPD% --movetimems %MTMS% --engine ./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe --engineoption "Hash=128" --outputpgn sf_wacnew_result.pgn --outputepd sf_wacnew_result.epd
+:: eap.exe --input %EPD% --movetimems %MTMS% --engine "./engine/stockfish-11-win/stockfish_20011801_x64_modern.exe" --engineoption "Hash=128" --outputpgn sf_wacnew_result.pgn --outputepd sf_wacnew_result.epd
 
 pause
